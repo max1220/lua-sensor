c1961bfcf0034a482b4157477d2a2c5adb88db36
Max <max@debian-fx>
2020-12-03 14:29:24 +0100

    cleaned up example

--- sensors/test.lua
+++ sensors/test.lua
@@ -1,5 +1,5 @@
-#!/usr/bin/lua
-local sens = require("sensors")
-for k,v in pairs(sens.readAll()) do
-	print(string.format("Sensor name: %s\n\tValue:%s\n", k, v))
+#!/usr/bin/lua5.1
+local sensors = require("sensors")
+for k,v in pairs(sensors.readAll()) do
+	print(k,v)
 end
