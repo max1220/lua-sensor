f50a014f48a5718a69b3a43e83187d439e4508c4
Max <max@debian-fx>
2020-12-03 14:02:59 +0100

    added separate init/cleanup functions; Added boolean skip init/cleanup argument to readAll function; Check for init errors

--- sensors/sensors.c
+++ sensors/sensors.c
@@ -8,13 +8,34 @@
 #include "lua.h"
 #include "lauxlib.h"
 
-#define LUA_LIB
 
 
+static int lua_sensor_init(lua_State *L) {
+	if (sensors_init(NULL)) {
+		lua_pushnil(L);
+		lua_pushstring(L, "Can't initialize sensors!");
+		return 2;
+	}
+	lua_pushboolean(L, 1);
+	return 1;
+}
 
-static int l_readAll(lua_State *L) {
+static int lua_sensor_cleanup(lua_State *L) {
+	sensors_cleanup();
+	lua_pushboolean(L, 1);
+	return 1;
+}
 
-	sensors_init(NULL);
+static int lua_sensor_readAll(lua_State *L) {
+	int skip_init_cleanup = 0;
+	if (lua_toboolean(L, 1)) {
+		skip_init_cleanup = 1;
+	} else if (sensors_init(NULL)) {
+		lua_pushnil(L);
+		lua_pushstring(L, "Can't initialize sensors!");
+		return 2;
+	}
+
 
 	const sensors_chip_name *scn;
 	const sensors_feature *sf;
@@ -23,46 +44,48 @@ static int l_readAll(lua_State *L) {
 	double r;
 	char scns[80];
 
-	lua_newtable(L);
 	//Push new table on to stack. Index -1
+	lua_newtable(L);
 
+	// Iterate over detected chips
 	for(n = 0; (scn = sensors_get_detected_chips(NULL, &n)) != NULL; ) {
-		// Iterate over detected chips
-
+		// Iterate over features of chip
 		for(n1 = 0; (sf = sensors_get_features(scn, &n1)) != NULL; )
-			// Iterate over features of chip
-
+			// Iterate over subfeatures of features from chip
 			for(n2 = 0; (ss = sensors_get_all_subfeatures(scn, sf, &n2)) != NULL; ) {
-				// Iterate over subfeatures of features from chip
-
-				sprintf(scns, "%s-%x-%x__%s", scn->prefix, scn->bus.nr, scn->addr, ss->name);
 				// Set scns to (chip.prefix - chip.bus.nr - chip.addr __ subfeature.name )
+				sprintf(scns, "%s-%x-%x__%s", scn->prefix, scn->bus.nr, scn->addr, ss->name);
 
-				err = sensors_get_value(scn, ss->number, &r);
 				// Get value from chip using subfeature.number, store result in r. If err != 0 then an error occured.
+				err = sensors_get_value(scn, ss->number, &r);
 
 				if (err == 0) {
-					// Sensor data. Addres in char* scns, name in ss->name, result in r
-					lua_pushnumber(L, r);																								//Push table value on stack. Table is now at -2
-					lua_setfield(L, -2, scns);																					//Set 3rd argument as key in table at stack index -2 to stack index -1
+					// No error, got Sensor data. Addres in char* scns, name in ss->name, result in r
+					lua_pushnumber(L, r); // Push table value on stack. Table is now at -2
+					lua_setfield(L, -2, scns); // Set 3rd argument as key in table at stack index -2 to stack index -1
 				}	else {
-					return(luaL_error(L, "Can't read sensors!"));
+					return luaL_error(L, "Can't read sensors!");
 				}
 			}
 	}
 
-	sensors_cleanup();
-	return(1);
+	if (skip_init_cleanup==0) {
+		sensors_cleanup();
+	}
+
+	return 1;
 }
 
-LUALIB_API int luaopen_sensors (lua_State *L) {
 
+LUALIB_API int luaopen_sensors (lua_State *L) {
 	static const struct luaL_reg lib_funcs [] = {
-      {"readAll", l_readAll},
-      {NULL, NULL}
-    };
+		{"readAll", lua_sensor_readAll},
+		{"init", lua_sensor_init},
+		{"cleanup", lua_sensor_cleanup},
+		{NULL, NULL}
+	};
 
 	luaL_openlib(L, "sensors", lib_funcs, 0);
 
-	return(1);
+	return 1;
 }
